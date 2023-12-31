fd52ced8a3f0a5b497da256ae533cc992494a549
mirzacolpa <colpamirza2@gmail.com>
2023-12-31 16:41:52 +0100

    Week3 Task2

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from restaurant import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
@@ -26,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+    #path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,17 @@
 #define URL route for index() view
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    
+    #path('restaurant/booking', )
+    #path('api-token-auth/', obtain_auth_token),
 ]
